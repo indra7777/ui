63a5217ec7a72c197cf2e2b98811673e803e8932
indra <98700452+indra7777@users.noreply.github.com>
2024-01-20 19:51:10 +0530

    contact route is created

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 dotenv.config()
-
 const app = express()
-//app.use(express.static(__dirname + '/public'));
-// app.use(express.static('public'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.set('view engine', 'ejs')
-// app.use('/public', express.static(__dirname + '/public'));
+
+//get request
+
+// home page
 app.get("/",(req,res)=>{
     res.render("index");
 })
+//hackathon page
 app.get('/hackathon',(req,res)=>{
     res.render('hackathon');
 })
 
+//login page
 app.get("/user",(req,res)=>{
     res.render('authentication')
 })
+
+//post requests
+
+//contact
+app.post("/contact", (req,res)=>{
+    //store  contact information in db
+    //code
+    res.render("index")
+})
+
+
+
+
 app.listen(process.env.PORT,()=>{
     console.log("server listening on port 3000")
 })
