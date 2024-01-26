3ef5bc5d84210354763f95f0292e7ac5bea696e2
indra <98700452+indra7777@users.noreply.github.com>
2024-01-26 21:37:45 +0530

    routes created

--- index.js
+++ index.js
@@ -42,6 +42,27 @@ app.get('/explore/solution',(req,res)=>{
 })
 
 //post requests
+app.post("/student/signup",(req,res)=>{
+    const {name,collegeName,registrationNumber,stream,branch,email,password,phone,currentYear,passedOutYear} = req.body
+    console.log(`name:${name} & email:${email} & password:${password} & phone:${phone}  & branch:${branch}
+    & stream:${stream} & collegeName:${collegeName} & registrationNumber:${registrationNumber}`)
+    
+    res.render('ps')
+})
+
+app.post("/industry/signup",(req,res)=>{
+  
+      const {name,industryName,employeeID,role,siteLink,email,password,officialNumber,personalNumber} = req.body
+    console.log(`name:${name} & email:${email} & password:${password} & phone:${officialNumber} & phone:${personalNumber}  & siteLink:${siteLink}
+    & role:${role} & employeeID:${employeeID} `)
+    res.render('ps')
+})
+app.post("/verify/signup",(req,res)=>{
+  
+      const {name,collegeName,designation,dept,areaOfExpertise,email,password,phone,experience} = req.body
+    console.log(`name:${name} & email:${email} & password:${password}  `)
+    res.render('ps')
+})
 
 //contact
 app.post("/contact", contactUS)
